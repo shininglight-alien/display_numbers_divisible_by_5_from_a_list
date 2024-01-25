34a0206bb538e7317891c1229eb1cf459df3caaa
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-25 11:16:31 +0800

    still for revision

---
+++ display_numbers_divisible_by_5_from_a_list.py
@@ -0,0 +1,11 @@
+# Display numbers divisible by 5 from a list
+
+def print_divisible_by_5(nums):
+    print("Given list is", nums)
+    print("Divisible by 5")
+    for num in nums:
+        if num % 5 == 0:
+            print(num)
+
+numbers = [10, 20, 33, 46, 55]
+print_divisible_by_5(numbers)
